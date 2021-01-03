ef990ae892042e7983e6acdf0ccbab4613097091
LinXiaoDe <qdl.cs@qq.com>
2021-01-03 14:49:31 +0800

    add 2

--- exe.sh
+++ exe.sh
@@ -1,1 +1,1 @@
-reflector -c China -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wireless_tools wpa_supplicant os-prober mtools dosfstools ntfs-3g base-devel linux-headers reflector git sudo
